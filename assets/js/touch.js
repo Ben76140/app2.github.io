f89e7a6470875755adbf119297c58549113d47fc
Ben Gentil <bengentil76@gmail.com>
2024-02-08 16:24:02 +0100

    pinch

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,5 +1,5 @@
 var touch = document.getElementById("touch");
-
+var debug = document.getElementById("debug");
 var hammertime = new Hammer(document.body, {});
 var ofLeft = touch.offsetLeft;
 var ofTop = touch.offsetTop;
@@ -10,6 +10,7 @@ hammertime.on('pan pinch', function(event) {
     let distance = event.distance;
     let additionalEvent = event.additionalEvent;
     console.log(additionalEvent,distance);
+    debug.innerText = additionalEvent;
     //"panright" "panleft" "panup" "pandown"
     switch (additionalEvent) {
         case "panright":
@@ -25,6 +26,9 @@ hammertime.on('pan pinch', function(event) {
             break;
         case "pandown":
             touch.style.transform = "translateY("+distance+"px)";
+        case "pinchin":
+
+            
             break;
     
         default:
